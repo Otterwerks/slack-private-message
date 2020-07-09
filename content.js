f34e21ccd36ecd2d8ba06ba09370a969f59cea42
Otterwerks <sweber102@gmail.com>
2020-07-09 15:29:54 -0700

    Laid more groundwork for key exchange UI

--- content.js
+++ content.js
@@ -21,17 +21,17 @@ const encryptButton = {
 	text: "Encrypt Message"
 };
 
-const diffieHellmanButton = `
-	<div id="dhbutton" class="btn-group dropup">
+var diffieHellmanButton = `
+	<div class="btn-group dropup">
   		<button type="button" class="c-button-unstyled c-icon_button c-icon_button--light p-composer__button p-composer__button--sticky dropdown-toggle" data-toggle="dropdown">
     		Secure Key Exchange
   		</button>
-		<div class="dropdown-menu px-1">
-			<button class="c-button-unstyled c-icon_button c-icon_button--light p-composer__button dropdown-item">Generate Computation</button>
+		<div class="dropdown-menu px-2">
+			<button id="slackPM_dh_generate" class="c-button-unstyled c-icon_button c-icon_button--light p-composer__button dropdown-item">Generate Computation</button>
 			<div class="dropdown-divider"></div>
-			<button class="c-button-unstyled c-icon_button c-icon_button--light p-composer__button dropdown-item">Import Friend's Computation</button>
+			<button id="slackPM_dh_importttttt" class="c-button-unstyled c-icon_button c-icon_button--light p-composer__button dropdown-item">Import Friend's Computation</button>
 			<div class="dropdown-divider"></div>
-			<button class="c-button-unstyled c-icon_button c-icon_button--light p-composer__button dropdown-item">Reset</button>
+			<button id="slackPM_dh_reset" class="c-button-unstyled c-icon_button c-icon_button--light p-composer__button dropdown-item">Reset</button>
 			<div class="dropdown-divider"></div>
   		</div>
 	</div>
@@ -48,7 +48,8 @@ const initializeDiffieHellmanButton = () => {
 	const buttonGroup = document.getElementsByClassName("p-composer__body");
 	if (buttonGroup.length > 0 && document.getElementById("dhButton") == null) {
 		buttonGroup[0].insertAdjacentHTML("beforeend", diffieHellmanButton);
-		document.getElementById("dhbutton").addEventListener("click", initiateDiffieHellman);
+		document.getElementById("slackPM_dh_generate").addEventListener("click", initiateDiffieHellman);
+		document.getElementById("slackPM_dh_reset").addEventListener("click", cleanupDiffieHellman);
 	};
 };
 
@@ -73,7 +74,7 @@ const messageObserverCallback = (mutationsList, observer) => {
 	};
 };
 
-const decryptExistingMessages = () => {
+const collectExistingMessages = () => {
 	const existingMessages = document.getElementsByClassName("c-virtual_list__item");
 	processNewMessages(existingMessages);
 }
@@ -105,7 +106,7 @@ const initialize = () => {
 	initializeEncryptionButton();
 	initializeDiffieHellmanButton();
 	initializeMessageObserver();
-	decryptExistingMessages();
+	collectExistingMessages();
 };
 
 // Secret Management
@@ -115,7 +116,7 @@ const getSecrets = () => {
 
 const addSecret = (secret) => {
 	window.localStorage.setItem("slackpm_secrets", JSON.stringify([secret]));
-	decryptExistingMessages();
+	collectExistingMessages();
 };
 
 // Helpers
@@ -138,8 +139,8 @@ const encryptionIndicator = "<<!>>";
 const encryptionSuccessNote = "SlackPM Secure Message: ";
 const encryptionFailureNote = "Unable to decrypt: ";
 
-const indicatorPresent = (string) => {
-	if (string.slice(0, 5) == encryptionIndicator) {
+const indicatorPresent = (stringToCheck, indicator) => {
+	if (stringToCheck.slice(0, indicator.length) == indicator) {
 		return true;
 	} else {
 		return false;
@@ -150,7 +151,7 @@ const encryptText = () => {
 	const secret = getSecrets()[0].value;
 	const message = readFromEditor('html')
 	const messageText = readFromEditor('text');
-	if (!indicatorPresent(messageText)) {
+	if (!indicatorPresent(messageText, encryptionIndicator)) {
 		if (messageText == "") {
 			return;
 		};
@@ -175,11 +176,25 @@ const decryptText = (encryptedMessage) => {
 const processNewMessages = (messageList) => {
 	for (let i = 0; i < messageList.length; i++) {
 		try {
-			const textElement = findTextElement(messageList[i]);
-			const encryptedMessage = textElement.textContent;
-			if (indicatorPresent(encryptedMessage)) {
-				const decryptedMessage = decryptText(encryptedMessage.slice(5));
-				textElement.innerHTML = decryptedMessage;
+			const encryptionMessageBlock = findTextElement(messageList[i], encryptionIndicator);
+			const diffieHellmanMessageBlock = findTextElement(messageList[i], diffieHellmanIndicator);
+			if (encryptionMessageBlock) {
+				const encryptedMessage = encryptionMessageBlock.textContent.slice(encryptionIndicator.length);
+				encryptionMessageBlock.innerHTML = decryptText(encryptedMessage);
+				console.log(ownSelf);
+			} else if (diffieHellmanMessageBlock) {
+				const ownSelf = document.getElementsByClassName("p-ia__sidebar_header__user__name")[0].textContent;
+				if (findTextElement(messageList[i], ownSelf)) { //removed ! for testing to allow on my own messages
+					const buttonValue = diffieHellmanMessageBlock.textContent.slice(diffieHellmanIndicator.length);
+					const keyName = messageList[i].getElementsByClassName("c-message__sender_link")[0].textContent;
+					const importButton = {
+						id: "slackPM_dh_import_" + buttonValue,
+						class: "btn btn-outline-secondary",
+						style: "",
+						text: "Import Computation"
+					};
+					renderButton(importButton, diffieHellmanMessageBlock, "beforeend", function() {completeDiffieHellman(buttonValue, keyName)})
+				};
 			};
 		} catch (error) {
 			continue;
@@ -187,12 +202,12 @@ const processNewMessages = (messageList) => {
 	};
 };
 
-const findTextElement = (node) => {
-    if (indicatorPresent(node.textContent)) {
+const findTextElement = (node, target) => {
+    if (indicatorPresent(node.textContent, target)) {
         return node;
     };
     for (let i = node.childNodes.length; i > 0; i--) {
-        let nextNode = findTextElement(node.childNodes[i-1]);
+        let nextNode = findTextElement(node.childNodes[i-1], target);
         if (nextNode != null) {
             return nextNode;
         };
@@ -201,13 +216,15 @@ const findTextElement = (node) => {
 }
 
 //Diffie Hellman (Group 18)
-var diffieHellmanG = bigInt(2);
-var diffieHellmanP = bigInt("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D120A92108011A723C12A787E6D788719A10BDBA5B2699C327186AF4E23C1A946834B6150BDA2583E9CA2AD44CE8DBBBC2DB04DE8EF92E8EFC141FBECAA6287C59474E6BC05D99B2964FA090C3A2233BA186515BE7ED1F612970CEE2D7AFB81BDD762170481CD0069127D5B05AA993B4EA988D8FDDC186FFB7DC90A6C08F4DF435C93402849236C3FAB4D27C7026C1D4DCB2602646DEC9751E763DBA37BDF8FF9406AD9E530EE5DB382F413001AEB06A53ED9027D831179727B0865A8918DA3EDBEBCF9B14ED44CE6CBACED4BB1BDB7F1447E6CC254B332051512BD7AF426FB8F401378CD2BF5983CA01C64B92ECF032EA15D1721D03F482D7CE6E74FEF6D55E702F46980C82B5A84031900B1C9E59E7C97FBEC7E8F323A97A7E36CC88BE0F1D45B7FF585AC54BD407B22B4154AACC8F6D7EBF48E1D814CC5ED20F8037E0A79715EEF29BE32806A1D58BB7C5DA76F550AA3D8A1FBFF0EB19CCB1A313D55CDA56C9EC2EF29632387FE8D76E3C0468043E8F663F4860EE12BF2D5B0B7474D6E694F91E6DBE115974A3926F12FEE5E438777CB6A932DF8CD8BEC4D073B931BA3BC832B68D9DD300741FA7BF8AFC47ED2576F6936BA424663AAB639C5AE4F5683423B4742BF1C978238F16CBE39D652DE3FDB8BEFC848AD922222E04A4037C0713EB57A81A23F0C73473FC646CEA306B4BCBC8862F8385DDFA9D4B7FA2C087E879683303ED5BDD3A062B3CF5B3A278A66D2A13F83F44F82DDF310EE074AB6A364597E899A0255DC164F31CC50846851DF9AB48195DED7EA1B1D510BD7EE74D73FAF36BC31ECFA268359046F4EB879F924009438B481C6CD7889A002ED5EE382BC9190DA6FC026E479558E4475677E9AA9E3050E2765694DFC81F56E880B96E7160C980DD98EDD3DFFFFFFFFFFFFFFFFF", 16);
+const diffieHellmanG = bigInt(2);
+const diffieHellmanP = bigInt("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D120A92108011A723C12A787E6D788719A10BDBA5B2699C327186AF4E23C1A946834B6150BDA2583E9CA2AD44CE8DBBBC2DB04DE8EF92E8EFC141FBECAA6287C59474E6BC05D99B2964FA090C3A2233BA186515BE7ED1F612970CEE2D7AFB81BDD762170481CD0069127D5B05AA993B4EA988D8FDDC186FFB7DC90A6C08F4DF435C93402849236C3FAB4D27C7026C1D4DCB2602646DEC9751E763DBA37BDF8FF9406AD9E530EE5DB382F413001AEB06A53ED9027D831179727B0865A8918DA3EDBEBCF9B14ED44CE6CBACED4BB1BDB7F1447E6CC254B332051512BD7AF426FB8F401378CD2BF5983CA01C64B92ECF032EA15D1721D03F482D7CE6E74FEF6D55E702F46980C82B5A84031900B1C9E59E7C97FBEC7E8F323A97A7E36CC88BE0F1D45B7FF585AC54BD407B22B4154AACC8F6D7EBF48E1D814CC5ED20F8037E0A79715EEF29BE32806A1D58BB7C5DA76F550AA3D8A1FBFF0EB19CCB1A313D55CDA56C9EC2EF29632387FE8D76E3C0468043E8F663F4860EE12BF2D5B0B7474D6E694F91E6DBE115974A3926F12FEE5E438777CB6A932DF8CD8BEC4D073B931BA3BC832B68D9DD300741FA7BF8AFC47ED2576F6936BA424663AAB639C5AE4F5683423B4742BF1C978238F16CBE39D652DE3FDB8BEFC848AD922222E04A4037C0713EB57A81A23F0C73473FC646CEA306B4BCBC8862F8385DDFA9D4B7FA2C087E879683303ED5BDD3A062B3CF5B3A278A66D2A13F83F44F82DDF310EE074AB6A364597E899A0255DC164F31CC50846851DF9AB48195DED7EA1B1D510BD7EE74D73FAF36BC31ECFA268359046F4EB879F924009438B481C6CD7889A002ED5EE382BC9190DA6FC026E479558E4475677E9AA9E3050E2765694DFC81F56E880B96E7160C980DD98EDD3DFFFFFFFFFFFFFFFFF", 16);
 var diffieHellmanSecretKey = null;
 var diffieHellmanFriendComputation = null;
 var diffieHellmanPending = false;
+const diffieHellmanIndicator = "<<?>>";
+ 
 
-const generateA = () => {
+const generateSecret = () => {
 	const hex = "0123456789ABCDEF";
 	var secretKey = "";
 	for (let i = 0; i < 512; i++) {
@@ -218,9 +235,9 @@ const generateA = () => {
 
 const initiateDiffieHellman = () => {
 	diffieHellmanPending = true;
-	diffieHellmanSecretKey = generateA();
-	const testFriendSecretKey = generateA();
-	const computation = diffieHellmanG.modPow(diffieHellmanSecretKey, diffieHellmanP).toString(36);
+	diffieHellmanSecretKey = generateSecret();
+	const testFriendSecretKey = generateSecret();
+	const diffieHellmanA = diffieHellmanG.modPow(diffieHellmanSecretKey, diffieHellmanP).toString(36);
 	const friendComputation = diffieHellmanG.modPow(testFriendSecretKey, diffieHellmanP).toString(36);
 	const sharedSecret = bigInt(friendComputation, 36).modPow(diffieHellmanSecretKey, diffieHellmanP);
 	const friendSharedSecret = bigInt(computation, 36).modPow(testFriendSecretKey, diffieHellmanP);
@@ -230,13 +247,19 @@ const initiateDiffieHellman = () => {
 	//writeToEditor(computation.toString(36).toUpperCase());
 };
 
-const completeDiffieHellman = () => {
-	
+const completeDiffieHellman = (diffieHellmanB, keyName) => {
+	if (!diffieHellmanPending) {
+		alert("Please generate and send a computation before completing the key exchange process.")
+		cleanupDiffieHellman();
+		return;
+	};
+	const sharedSecret = bigInt(diffieHellmanB, 36).modPow(diffieHellmanSecretKey, diffieHellmanP);
+	//save key entry
 	cleanupDiffieHellman();
 };
 
 const cleanupDiffieHellman = () => {
-	diffieHellmanP = false;
+	diffieHellmanPending = false;
 	diffieHellmanSecretKey = null;
 	diffieHellmanFriendComputation = null;
 };
